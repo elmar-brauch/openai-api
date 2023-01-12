2410576995bbd27d010d618f0f06836c7b72bc8f
Elmar Brauch <elmar.brauch@gmail.com>
2023-01-12 20:35:34 +0100

    ...

--- src/main/java/de/bsi/chatgpt/CompletionRequest.java
+++ src/main/java/de/bsi/chatgpt/CompletionRequest.java
@@ -1,6 +1,7 @@
 package de.bsi.chatgpt;
 
-public record CompletionRequest(String model, String prompt, double temperature, int max_tokens) {
+public record CompletionRequest(String model, String prompt, 
+		double temperature, int max_tokens) {
 	
 	public static CompletionRequest defaultWith(String prompt) {
 		return new CompletionRequest("text-davinci-003", prompt, 0.7, 100);
